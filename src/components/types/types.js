db867608d9929fdd7dfa53dce5c5b29330043ca4
Nico Olivera <oliveran9@gmail.com>
2023-01-21 11:25:29 -0300

    inicializo repo

---
+++ src/components/types/types.js
@@ -0,0 +1,19 @@
+
+export const types= {
+    login:'[Auth] Login',
+    logout: '[Auth] Logout',
+
+    uiSetError: '[UI] Set error',
+    uiRemoveError: '[UI] Remove Error',
+
+    uiStartLoading: '[UI] Start loading',
+    uiFinishLoading: '[UI] Finish loading',
+
+    notesAddNew: '[Notes] New Note',
+    notesActive: '[Notes] Set active note',
+    notesLoad: '[Notes] load Notes',
+    notesUpdated: '[Notes] update note',
+    notesFileUrl: '[Notes] update image url',
+    notesDelete: '[Notes] Delete note',
+    notesLogoutCleaning: '[notes] logout Cleaning'
+}
---
+++ src/hooks/useForm.js
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+
+export const useForm = ( initialState = {} ) => {
+    
+    const [values, setValues] = useState(initialState);
+
+    const reset = ( newFormState = initialState) => {
+        setValues( newFormState );
+    }
+
+
+    const handleInputChange = ({ target }) => {
+
+        setValues({
+            ...values,
+            [ target.name ]: target.values
+        });
+
+    }
+
+    return [ values, handleInputChange, reset ];
+
+}
